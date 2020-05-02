0b96959dc3eea8828afe5638ff9ba0ea218dc2ac
c-champagne <cassielchampagne@gmail.com>
2020-05-02 09:30:10 -0400

    Finished exercise 2

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res, next) {
     res.send('Hello, world!')
@@ -19,23 +19,39 @@ app.get('/cats', function (req, res, next) {
 });
 
 app.get('/dogs', function (req, res, next) {
-    res.send('Woof!')
+    /* res.send('Woof!') */
+    res.render('dogs', {
+        pageTitle: 'Who\'s a good boy?',
+        greeting: 'Woof!'
+    })
 });
 
 app.get('/cats_and_dogs', function (req, res, next) {
-    res.send('Living together')
+    /* res.send('Living together') */
+    res.render('together', {
+        pageTitle: 'In harmony',
+        greeting: 'Woof-meow!'
+    })
 });
 
 app.get('/greet/:userName', function (req, res, next){
     const userName = req.params.userName;
-    res.send(`Hello, ${userName}`);
+    /* res.send(`Hello, ${userName}`); */
+    res.render('user', {
+        pageTitle: 'Must have thumbs to use this page',
+        greeting: 'Hello, ' + userName + '!'
+    })
 })
 
 app.get('/year', function(req, res, next){
     let age = req.query.age;
     let birthYear = 2020 - age;
-
-    res.send(`You were born in ${birthYear}`);
+    //query parameter is /year?age=27
+    /* res.send(`You were born in ${birthYear}`); */
+    res.render('year', {
+        pageTitle:'Forget what year you were born?',
+        greeting: 'You were born in ' + birthYear
+    })
 })
 
 
